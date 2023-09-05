d129d1f6eaad25e99c2620b8af487a452cd7f428
NikolaiKorotonozhkin <anikola1k@gmail.com>
2023-09-05 19:29:23 +0300

    second Task

---
+++ task2/task2/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  task2
+//
+//  Created by Nikolai  on 05.09.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+    
+}
